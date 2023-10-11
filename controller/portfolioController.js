1e14de19919be97e340040dae968f6fcb8d956be
hasan <hasanocal7@gmail.com>
2023-10-11 11:39:04 +0300

    Added Image Uploader with Multer

--- controller/portfolioController.js
+++ controller/portfolioController.js
@@ -1,22 +1,31 @@
 const { Portfolios } = require('../models');
+const multer = require('multer');
 
 exports.createPortfolio = async (req, res) => {
   try {
-    const uploadedImage = req.file;
-    if (!uploadedImage) {
-      console.error('Dosya yüklenemedi.');
-      return res.status(400).send('Dosya yüklenemedi.');
+    if (req.file instanceof multer.MulterError) {
+      console.error('Resim yüklenirken Multer kaynaklı hata oluştu');
+      return res.status(400).send('Resim yüklenirken hata oluştu');
+    } else if (!req.file) {
+      console.error('Resim yüklenemedi veya eksik');
+      return res.status(400).send('Lütfen bir resim yükleyin');
     }
-    await Portfolios.create({
+
+    const imageFileName = 'uploads/' + req.file.filename;
+
+    const portfolio = await Portfolios.create({
       ...req.body,
-      image: '/uploads/' + uploadedImage.filename,
+      image: imageFileName,
     });
-    res.redirect('/');
+
+    console.log(`${portfolio.image} Başarılı bir şekilde yüklendi`);
+
+    return res.redirect('/');
   } catch (error) {
-    console.error('Portföy oluşturulamadı:', error);
-    res.status(500).send('Portföy oluşturulamadı.');
+    console.error(error);
+    return res.status(500).send('Bir hata oluştu');
   }
-}
+};
 
 
 exports.getOnePortfolio = async (req, res) => {
