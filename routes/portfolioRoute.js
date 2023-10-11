e186675a32ec7be5071a4e4987f3218027c05849
hasan <hasanocal7@gmail.com>
2023-10-11 11:39:04 +0300

    Added Image Uploader with Multer

--- routes/portfolioRoute.js
+++ routes/portfolioRoute.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const portfolioController = require('../controller/portfolioController');
+
+const fs = require('fs');
+const path = require('path');
 const multer = require('multer');
-const upload = multer({ dest: 'public/uploads/' }); 
-const portfolioController = require('../controller/portfolioController')
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      const rootDir = path.dirname(require.main.filename)
+      console.log("require.main.filename : ",require.main.filename);
+
+      fs.mkdirSync(path.join(rootDir, "/public/uploads"), { recursive: true })
+      cb(null, path.join(rootDir, "/public/uploads"))
+    },
+    filename: function (req, file, cb) {
+      const extension = file.mimetype.split("/")[1]
+
+      if (!req.savedImages) req.savedImages = []
+
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+
+      let url = `image_${uniqueSuffix}.${extension}`
+
+      req.savedImages = [...req.savedImages, path.join(url)]
+
+      cb(null, url)
+    }
+  })
+});
 
 //router.get("/", portfolioController.getOnePortfolio);
 router.post("/", upload.single('image'), portfolioController.createPortfolio);
